c057275fb7a755f801f8fd7b714017e05f9a9881
RyseSlade <github@aedon.de>
2021-02-13 14:41:08 +0100

    Add event name

--- src/Event/Discord/Resumed.php
+++ src/Event/Discord/Resumed.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Aedon\DiscordBot\Event\Discord;
 
 use Aedon\DiscordBot\Event\AbstractInternalEvent;
+use Aedon\DiscordBot\Event\EventInterface;
 
 final class Resumed extends AbstractInternalEvent
 {
-
+    public function getName(): string
+    {
+        return EventInterface::RESUMED;
+    }
 }
