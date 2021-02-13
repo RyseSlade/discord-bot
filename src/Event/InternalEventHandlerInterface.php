f771a0ddbce6bc6869ab706787c8e340ae07da8e
RyseSlade <github@aedon.de>
2021-02-13 14:42:05 +0100

    Change return values

--- src/Event/InternalEventHandlerInterface.php
+++ src/Event/InternalEventHandlerInterface.php
@@ -9,8 +9,8 @@ use React\EventLoop\LoopInterface;
 
 interface InternalEventHandlerInterface
 {
-    public function setWebSocket(WebSocket $webSocket): self;
-    public function setLoop(LoopInterface $loop): self;
+    public function setWebSocket(WebSocket $webSocket): void;
+    public function setLoop(LoopInterface $loop): void;
     public function updateSequenceNumber(int $sequenceNumber): void;
     public function process(EventInterface $event): void;
 }
